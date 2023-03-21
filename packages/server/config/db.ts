a0ba4a63481e859078e904dbdac6fc6d7c344442
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-21 17:08:19 +0530

    create the socket connections

--- packages/server/config/db.ts
+++ packages/server/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
@@ -15,4 +15,3 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
