62ecbb0459a1fecf9cdb82f30ee128e784a7ea2f
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-21 16:14:37 +0530

    add the chat pages

--- packages/chatify-app/src/pages/ChatPage.tsx
+++ packages/chatify-app/src/pages/ChatPage.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { ChatState } from '../Context/ChatProvider';
+import { Box } from "@chakra-ui/layout";
+import MyChats from '../components/MyChats';
+import Chatbox from '../components/Chatbox';
+import SideDrawer from '../components/miscellaneous/SideDrawer';
 function ChatPage() {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+
   return (
-    <div>ChatPage</div>
-  )
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
+    </div>
+  );
 }
 
 export default ChatPage
