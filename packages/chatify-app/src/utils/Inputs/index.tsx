3228ce18ff7f9d4cb610a11cfbd1c40351681693
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-22 10:36:00 +0530

    add the react query api initiations

--- packages/chatify-app/src/utils/Inputs/index.tsx
+++ packages/chatify-app/src/utils/Inputs/index.tsx
@@ -26,6 +26,8 @@ export const InputComponent = ({
         <input
           placeholder={placeholder}
           type={type}
+          value={value}
+          onChange={onChange}
           className="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"
