503ef684278e1c8416fd06357022480348e6aa54
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-22 10:43:21 +0530

    feat: create the userData sections

--- packages/chatify-app/src/components/Authentication/Signup/signup.tsx
+++ packages/chatify-app/src/components/Authentication/Signup/signup.tsx
@@ -1,38 +1,42 @@
-import { InputComponent } from '../../../utils/Inputs'
+import { useState } from "react";
+import { InputComponent } from "../../../utils/Inputs";
 
 export function SignUp() {
+  const [userData, setUserData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profilePic: "",
+  });
+  function handleUserData(key,e){
+    
+  }
   return (
-     <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
-     <InputComponent
-       type="text"
-       label="Username"
-       placeholder="Guru"
-     />
-     <InputComponent
-       type="email"
-       label="Email"
-       placeholder="nikilmethew@gmail.com"
-     />
-     <InputComponent
-       type="password"
-       label="Password"
-       placeholder="Password"
-     />
-     <InputComponent
-       type="password"
-       label="Password"
-       placeholder="Confirm Password"
-     />
+    <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
+      <InputComponent type="text" label="Username" placeholder="Guru" />
+      <InputComponent
+        type="email"
+        label="Email"
+        placeholder="nikilmethew@gmail.com"
+        value={userData.userName}
+        onChange={(e)=>handleUserData("userName",e)}
+      />
+      <InputComponent type="password" label="Password" placeholder="Password" />
+      <InputComponent
+        type="password"
+        label="Password"
+        placeholder="Confirm Password"
+      />
 
-     <div className="relative">
-       <a
-         className="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-orange-700
+      <div className="relative">
+        <a
+          className="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-orange-700
 rounded-lg transition duration-200 hover:bg-orange-500 ease"
-       >
-         Sign Up
-       </a>
-     </div>
-   </div>
-  )
+        >
+          Sign Up
+        </a>
+      </div>
+    </div>
+  );
 }
-
