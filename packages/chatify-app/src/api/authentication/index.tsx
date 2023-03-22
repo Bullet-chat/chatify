5836156876a4c4d0085eb455ccb923f7b96b888a
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-22 18:19:29 +0530

    add the react query in the sign in sections

--- packages/chatify-app/src/api/authentication/index.tsx
+++ packages/chatify-app/src/api/authentication/index.tsx
@@ -1,29 +1,45 @@
 import axios from "axios";
 
 interface UserProps {
-  name: string;
+  name?: string;
   email: string;
   password: string;
-  pic: string;
+  pic?: string;
 }
+
+const config = {
+  headers: {
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+  },
+};
 export const CreateUser = async (data: UserProps) => {
   const { name, email, password, pic } = data;
-  console.log(data);
-  const config = {
-    headers: {
-      "Content-type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true",
-    },
-  };
+
   return await axios
     .post(
-      "http://localhost:5000/api/user",
+      `${import.meta.env.VITE_BACKEND_API}/api/user`,
       {
         name,
         email,
         password,
         pic,
+      },
+      config
+    )
+    .then((res) => res.data);
+};
+
+export const SignInUser=async (data: UserProps) => {
+  const { email, password } = data;
+
+  return await axios
+    .post(
+      `${import.meta.env.VITE_BACKEND_API}/api/user/login`,
+      {
+        email,
+        password
       },
       config
     )
