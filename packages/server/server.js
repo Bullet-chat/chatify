ea3b78b531140e31fc42b04c115719ae6a90ba1d
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-22 09:27:52 +0530

    feat: config all sections with server files

--- packages/server/server.js
+++ packages/server/server.js
@@ -3,19 +3,39 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const connect=require("./config/db");
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-
+const path = require("path");
 
 dotenv.config();
 connect();
 const app = express();
 
 app.use(express.json()); // to accept json data
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+// --------------------------deployment------------------------------
 
+const __dirname1 = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
+// --------------------------deployment------------------------------
+
+app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT||3000;
 
