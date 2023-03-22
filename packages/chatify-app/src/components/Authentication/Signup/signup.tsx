8e5f662878500be0bbd5dce4d3bafb06d2585c3e
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-22 10:59:14 +0530

    implement the handle userData

--- packages/chatify-app/src/components/Authentication/Signup/signup.tsx
+++ packages/chatify-app/src/components/Authentication/Signup/signup.tsx
@@ -18,31 +18,49 @@ export function SignUp() {
   function handleUserData(key: string, e: React.ChangeEvent<HTMLInputElement>) {
     console.log("data");
     setUserData((prev: userDataType) => ({
-      ...prev,
       [key]: e.target.value,
+      ...prev,
     }));
+  }
+  function SubmitHandler() {
+    console.log(userData);
   }
   return (
     <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
-      <InputComponent type="text" label="Username" placeholder="Guru" />
+      <InputComponent
+        type="text"
+        label="Username"
+        placeholder="Guru"
+        value={userData.userName}
+        onChange={(e) => handleUserData("userName", e)}
+      />
       <InputComponent
         type="email"
         label="Email"
         placeholder="nikilmethew@gmail.com"
-        value={userData.userName}
-        onChange={(e) => handleUserData("userName", e)}
+        value={userData.email}
+        onChange={(e) => handleUserData("email", e)}
       />
-      <InputComponent type="password" label="Password" placeholder="Password" />
+      <InputComponent
+        type="password"
+        label="Password"
+        placeholder="Password"
+        value={userData.password}
+        onChange={(e) => handleUserData("password", e)}
+      />
       <InputComponent
         type="password"
         label="Password"
         placeholder="Confirm Password"
+        value={userData.confirmPassword}
+        onChange={(e) => handleUserData("confirmPassword", e)}
       />
 
       <div className="relative">
         <a
           className="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-orange-700
 rounded-lg transition duration-200 hover:bg-orange-500 ease"
+          onClick={SubmitHandler}
         >
           Sign Up
         </a>
