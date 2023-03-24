5e4aa137b7718b0a66c905261656ec3929dd0955
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-24 10:17:20 +0530

    feat: create the exprts sections

---
+++ packages/chatify-app/src/components/userDetails/index.ts
@@ -0,0 +1,7 @@
+import { UserDetails } from "./userDetails";
+import { ChatRoomInfo } from "./ChatRoomInfo";
+interface userDetailsProps {
+  user?: any;
+}
+export { UserDetails, ChatRoomInfo };
+export type { userDetailsProps };
