04dfaabaa0e887ee7738662441a595d96be5e0e7
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-28 09:23:37 +0530

    handle ai conversations

--- packages/chatify-app/src/components/ChatWithAI/ChatWithAI.tsx
+++ packages/chatify-app/src/components/ChatWithAI/ChatWithAI.tsx
@@ -1,10 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import { ChatState } from "../../Context/ChatProvider";
 
 export function ChatWithAI() {
+  const { isAIConversation, setIsAIConversation } = ChatState();
+  function handleAIChat() {
+    setIsAIConversation(!isAIConversation);
+  }
   return (
     <Box
       className="flex  text-center bg-slate-500 justify-center cursor-pointer"
-      onClick={() => {console.log("chatai")}}
+      onClick={handleAIChat}
     >
       <Box className="flex items-center text-white font-medium tracking-wider">
         ChatWithAI
