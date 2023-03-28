7da2b3bc955e23366f8afe6850382c4aa0cf760a
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-28 11:33:53 +0530

    create the ai controllers

--- packages/server/routes/aiRoutes.js
+++ packages/server/routes/aiRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { talkWithAi } = require("../controllers/aiControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
+router.route("/").post(protect, talkWithAi);
 
 module.exports = router;
