ca4f63693cf70f96abf47b50c1ca979ea6c08ac6
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-28 10:47:06 +0530

    add the typing handler sections

--- packages/chatify-app/src/components/ChatWithAI/AiChatroom.tsx
+++ packages/chatify-app/src/components/ChatWithAI/AiChatroom.tsx
@@ -1,10 +1,48 @@
-import React from "react";
+import { Box, FormControl, Input } from "@chakra-ui/react";
+import React, { SetStateAction, useState } from "react";
+import { ChatState } from "../../Context/ChatProvider";
+import { Colors } from "../../utils/Colors";
 interface Props {
   fetchAgain: boolean;
   setFetchAgain: (args: boolean) => void;
 }
 function AiChatroom({ fetchAgain, setFetchAgain }: Props) {
-  return <div>AiChatroom</div>;
+  const { isAIConversation } = ChatState();
+  const [newMessage, setNewMessage] = useState("");
+  function sendMessageToBot() {
+    console.log("called");
+  }
+  function typingHandler(e: { target: { value: SetStateAction<string> } }){
+     setNewMessage(e.target.value)
+     return;
+  }
+  return (
+    <Box
+      display={{ base: isAIConversation ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg={Colors.mainPrimary}
+      width="100%"
+      borderWidth="1px"
+    >
+      <FormControl
+        onKeyDown={sendMessageToBot}
+        id="Ai-connections"
+        isRequired
+        mt={3}
+      >
+        <Input
+          variant="filled"
+          bg="#E0E0E0"
+          placeholder="Enter a message.."
+          value={newMessage}
+          color={Colors.mainSecondary}
+          onChange={typingHandler}
+        />
+      </FormControl>
+    </Box>
+  );
 }
 
 export default AiChatroom;
