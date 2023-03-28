18b7943a87f8355f398fabb5f06441ce12fb756e
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-28 18:03:47 +0530

    default export sections

--- packages/chatify-app/src/utils/generateUniqueId.tsx
+++ packages/chatify-app/src/utils/generateUniqueId.tsx
@@ -1,4 +1,4 @@
-export function generateUniqueId() {
+export default function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
