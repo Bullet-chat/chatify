2485c100919db90f9b26aaed3d9114eb4bc3e017
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-29 13:48:10 +0530

    remove the primary color

--- packages/chatify-app/src/components/MyChats.tsx
+++ packages/chatify-app/src/components/MyChats.tsx
@@ -10,14 +10,16 @@ import { Button } from "@chakra-ui/react";
 import { ChatState } from "../Context/ChatProvider";
 import { Colors } from "../utils/Colors";
 import { ChatWithAI } from "./ChatWithAI";
+import { UsersListProps } from "../types/userTypes";
 interface Props {
   fetchAgain: boolean;
 }
+
 const MyChats = ({ fetchAgain }: Props) => {
   const [loggedUser, setLoggedUser] = useState();
 
   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
-  
+
   const toast = useToast();
 
   const fetchChats = async () => {
@@ -53,73 +55,84 @@ const MyChats = ({ fetchAgain }: Props) => {
     );
     if (user.token) fetchChats();
   }, [fetchAgain, user.token]);
-
+  console.log("chatss====>", chats);
   return (
     <Box
       display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
       flexDir="column"
       alignItems="center"
       p={3}
-      bg={Colors.mainPrimary}
       w={{ base: "100%", md: "31%" }}
       borderWidth="1px"
     >
-        <Text className="text-4xl text-black font-bold font-serif cursor-pointer text-left w-full">
-          Chatify
-        </Text>
+      <Text className="text-4xl text-black font-bold font-sofia cursor-pointer text-left w-full">
+        Chatify
+      </Text>
       <Box
         pb={3}
         px={3}
-        // fontSize={{ base: "28px", md: "30px" }}
-        // fontFamily="Work sans"
         display="flex"
         w="100%"
         justifyContent="space-between"
         alignItems="center"
-      className="text-black text-lg font-medium"
+        className="text-[#4B5155] text-base font-medium font-sofia"
       >
-        Messages
+        Active Chats
         <GroupChatModal>
-          <AddIcon w={4} h={4} color="green.500"/>
+          <AddIcon w={3} h={3} color="gray.400" />
         </GroupChatModal>
       </Box>
       <Box
         display="flex"
         flexDir="column"
         p={3}
-        bg={Colors.mainPrimary}
         w="100%"
         h="100%"
         borderRadius="lg"
         overflowY="hidden"
       >
-        <ChatWithAI/>
+        <ChatWithAI />
         {chats ? (
           <Stack overflowY="scroll">
             {chats.map((chat: any) => (
               <Box
                 onClick={() => setSelectedChat(chat)}
                 cursor="pointer"
-                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
                 color={selectedChat === chat ? "white" : "black"}
                 px={3}
                 py={2}
                 borderRadius="lg"
                 key={chat._id}
+                className={`${
+                  selectedChat === chat
+                    ? "bg-[#F9FAFC] border-[#DBE5ED] border-2 rounded-md"
+                    : "bg-white"
+                } flex`}
               >
-                <Text>
-                  {!chat.isGroupChat
-                    ? getSender(loggedUser, chat.users)
-                    : chat.chatName}
-                </Text>
-                {chat.latestMessage && (
-                  <Text fontSize="xs">
-                    <b>{chat.latestMessage.sender.name} : </b>
-                    {chat.latestMessage.content.length > 50
-                      ? chat.latestMessage.content.substring(0, 51) + "..."
-                      : chat.latestMessage.content}
+                <Box className="relative mr-5">
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src="https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                    alt=""
+                  />
+                  <span className="bottom-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                </Box>
+
+                <Box>
+                  <Text className="text-[#4B5155] font-semibold font-sofia text-base">
+                    {!chat.isGroupChat
+                      ? getSender(loggedUser, chat.users)
+                      : chat.chatName}
                   </Text>
-                )}
+                  {chat.latestMessage && (
+                    <Text className="text-[#7B8793] font-semibold text-xs">
+                      {chat.latestMessage.sender.name} :
+                      {chat.latestMessage.content.length > 50
+                        ? chat.latestMessage.content.substring(0, 51) + "..."
+                        : chat.latestMessage.content}
+                    </Text>
+                  )}
+                </Box>
               </Box>
             ))}
           </Stack>
