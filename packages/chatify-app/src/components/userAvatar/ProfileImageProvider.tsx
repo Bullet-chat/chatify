2b18d90737fca53bb60e0221516fa4bfe2263715
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-30 19:04:30 +0530

    add the onerror image loading sections

--- packages/chatify-app/src/components/userAvatar/ProfileImageProvider.tsx
+++ packages/chatify-app/src/components/userAvatar/ProfileImageProvider.tsx
@@ -17,7 +17,15 @@ function ProfileImageProvider({ userItem }: any) {
   }, [userItem]);
   return (
     <Box className="relative mr-5">
-      <img className="w-10 h-10 rounded-full" src={userImage} alt="" />
+      <img
+        className="w-10 h-10 rounded-full"
+        src={userImage}
+        alt=""
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null; // prevents looping
+          currentTarget.src = defaultUserImage;
+        }}
+      />
       <span className="bottom-0 left-7 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
     </Box>
   );
