f203d1fa3da8f3997eae335056e91ba0f7413fc0
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-30 17:58:44 +0530

    fix the button designs in the chat with ai sections

--- packages/chatify-app/src/components/ChatWithAI/ChatWithAI.tsx
+++ packages/chatify-app/src/components/ChatWithAI/ChatWithAI.tsx
@@ -13,10 +13,10 @@ export function ChatWithAI() {
   }
   return (
     <Box
-      className="flex  text-center bg-slate-500 justify-center cursor-pointer"
+      className="flex py-2 rounded-lg mb-4  text-center bg-slate-500 justify-center cursor-pointer"
       onClick={handleAIChat}
     >
-      <Box className="flex items-center text-white font-medium tracking-wider">
+      <Box className="flex items-center text-white font-medium tracking-wider font-sofia">
         ChatWithAI
       </Box>
     </Box>
